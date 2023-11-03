67e0ff160a688bc46cf002b92aeb7cb9cc56fb90
Minhal Vakil <minhal@vakil.ca>
2023-11-03 15:44:20 -0400

    Updated Character Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target;
+    private Vector3 initialOffset;
+    [SerializeField]
+    float cameraFolllowSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
