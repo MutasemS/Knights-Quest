18ffa74ca945d54c6c1ec78953f2230d5eca05cd
Minhal Vakil <minhal@vakil.ca>
2023-11-05 22:55:40 -0500

    Added Start Scene

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Entity : MonoBehaviour
+{
+    public float health;
+    protected float maxHealth;
+    // Use this for initialization
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void damage(float d)
+    {
+        this.health -= d;
+        FindObjectOfType<AudioManager>().Play("Hurt");
+        OnDamage();
+        if (this.health<=0)
+        {
+            this.DestroyThis();
+        }
+        
+    }
+    public abstract void DestroyThis();
+    public abstract void OnDamage();
+}
