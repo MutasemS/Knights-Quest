3dde273e141fb4595461b61ae724e3c376cf55e1
Mutasem Salem <ms5268@drexel.edu>
2023-11-15 13:43:05 -0500

    Changes in pause script

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -24,7 +24,7 @@ public class PauseMenu : MonoBehaviour
 
     public void Restart()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
 
     }
